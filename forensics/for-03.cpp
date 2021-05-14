71a8699f3f64cc5c9f649771cba43886a952e303
arhammusheer <arhammusheer@gmail.com>
2021-05-14 21:43:54 +0530

    FOrensics

---
+++ forensics/for-03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	cout<<"pbqrz";
+	cout<<"vna";
+	cout<<"PGS{EB";
+	cout<<"G13";
+	cout<<"_vF_";
+	cout<<"a1p3}";
+	return 0;
+}
