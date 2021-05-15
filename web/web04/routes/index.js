94fac2536234a475fc38ab925fec1a892f60d862
arhammusheer <arhammusheer@gmail.com>
2021-05-15 19:51:41 +0530

    authorization patch

--- web/web04/routes/index.js
+++ web/web04/routes/index.js
@@ -49,27 +49,38 @@ router.post("/login", async function (req, res, next) {
   );
 });
 
-router.get("/user", function (req, res, next){
-  token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, publicKey, function(err, decoded) {
-    res.json({status:"success", message: `Hi ${decoded.name}`})
+router.get("/user", function (req, res, next) {
+  if (!req.headers.authorization)
+    return res.json({ status: "warning", message: "No token provided" });
+  token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, publicKey, function (err, decoded) {
+    res.json({ status: "success", message: `Hi ${decoded.name}` });
   });
-})
+});
 
-router.get("/admin", function (req, res, next){
-  token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, publicKey, function(err, decoded) {
-    if(decoded.name !=="admin") return res.json({status:"success", message: `No flag for you. You ain't admin`})
-    res.json({status:"success", flag:"Y29kZW1pYW5DVEZ7WW9VX1MzRW1fTDFrM19BX1ByMH0="})
+router.get("/admin", function (req, res, next) {
+  if (!req.headers.authorization)
+    return res.json({ status: "warning", message: "No token provided" });
+  token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, publicKey, function (err, decoded) {
+    if (decoded.name !== "admin")
+      return res.json({
+        status: "success",
+        message: `No flag for you. You ain't admin`,
+      });
+    res.json({
+      status: "success",
+      flag: "Y29kZW1pYW5DVEZ7WW9VX1MzRW1fTDFrM19BX1ByMH0=",
+    });
   });
-})
+});
 
-router.get("/robots.txt", function (req, res, next){
-  res.sendFile(path.join(__dirname,"../robots.txt"))
-})
+router.get("/robots.txt", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../robots.txt"));
+});
 
-router.get("/private.pem", function (req, res, next){
-  res.sendFile(path.join(__dirname, "../keys/jwtRS256.key"))
-})
+router.get("/private.pem", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../keys/jwtRS256.key"));
+});
 
 module.exports = router;
